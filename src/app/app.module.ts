f85ea6442ebb3e99f439bc21693466c40c1dfce6
mingxiangchan <mingxiangchan@gmail.com>
2019-03-05 15:48:57 +0800

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component'
+import { UserListComponent } from './user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [AppComponent, UserListComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
